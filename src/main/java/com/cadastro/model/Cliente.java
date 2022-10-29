b1c4161fe9a12653f648c626415d5edf06efbb24
Júlio César Fragoso de Lima <103080100+chakaartt@users.noreply.github.com>
2022-10-29 00:23:50 -0300

    29/10/22

--- src/main/java/com/cadastro/model/Cliente.java
+++ src/main/java/com/cadastro/model/Cliente.java
@@ -31,5 +31,38 @@ public class Cliente {
 	@Column(name = "senha_cliente")
 	private String senha_cliente;
 
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome_cliente() {
+		return nome_cliente;
+	}
+
+	public void setNome_cliente(String nome_cliente) {
+		this.nome_cliente = nome_cliente;
+	}
+
+	public String getEmail_cliente() {
+		return email_cliente;
+	}
+
+	public void setEmail_cliente(String email_cliente) {
+		this.email_cliente = email_cliente;
+	}
+
+	public String getSenha_cliente() {
+		return senha_cliente;
+	}
+
+	public void setSenha_cliente(String senha_cliente) {
+		this.senha_cliente = senha_cliente;
+	}
+
+	
 	
 }
--- src/main/java/com/cadastro/serviceImpl/ClienteServiceImpl.java
+++ src/main/java/com/cadastro/serviceImpl/ClienteServiceImpl.java
@@ -1,5 +1,57 @@
 package com.cadastro.serviceImpl;
 
-public class ClienteServiceImpl {
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.cadastro.exception.ResourceNotFoudException;
+import com.cadastro.model.Cliente;
+import com.cadastro.repositories.ClienteRepository;
+import com.cadastro.service.ClienteService;
 
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+	private ClienteRepository clienteRepository;
+
+	public ClienteServiceImpl(ClienteRepository clienteRepository) {
+		super();
+		this.clienteRepository = clienteRepository;
+	}
+	
+	
+	public Cliente saveCliente(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	
+	public List<Cliente> getAllCliente() {
+		return clienteRepository.findAll();
+	}
+	
+	
+	public Cliente getClienteById(long id_cliente) {
+		return clienteRepository.findById(id_cliente)
+				.orElseThrow(() -> new ResourceNotFoudException("Cliente", "id_cliente", id_cliente));
+	}
+	
+	
+	public Cliente updatecliente(Cliente cliente, long id_cliente) {
+
+		Cliente clienteExistente = clienteRepository.findById(id_cliente)
+				.orElseThrow(() -> new ResourceNotFoudException("Cliente", "id_cliente", id_cliente));
+
+		clienteExistente.setNome_cliente(cliente.getNome_cliente());
+		clienteExistente.setNome_cliente(cliente.getEmail_cliente());
+		clienteExistente.setNome_cliente(cliente.getSenha_cliente());
+
+		clienteRepository.save(clienteExistente);
+		return clienteExistente;
+	}
+	
+	
+	public void deleteCliente(long id_cliente) {
+
+		clienteRepository.findById(id_cliente)
+				.orElseThrow(() -> new ResourceNotFoudException("cliente", "id_cliente", id_cliente));
+		clienteRepository.deleteById(id_cliente);
+	}
 }
